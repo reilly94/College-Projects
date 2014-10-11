f00ff95f24f8ebe6bd1d35f5f5dce29a84db69f9
reilly94 <stephen-r-94@hotmail.com>
2014-10-11 21:04:08 +0100

    College projects

---
+++ Team C OOP Project Upload/Team C OOP Project/Images/ComputerSupportApp/SENDTODAVE/ComputerSupportApplication/src/computersupportapplication/ComputerSupportApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computersupportapplication;
+
+/**
+ *
+ * @author John Mc Dermott
+ */
+public class ComputerSupportApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+       ComputerSupportGUI myCSGUI = new ComputerSupportGUI();
+       myCSGUI.setVisible(true);
+    
+    }
+}
+
