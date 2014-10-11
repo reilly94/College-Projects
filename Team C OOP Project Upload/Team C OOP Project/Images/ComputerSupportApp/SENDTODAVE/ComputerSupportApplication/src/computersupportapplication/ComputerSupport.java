07165d73d4a8446f6fa98c49786ecff86ecf1ddb
reilly94 <stephen-r-94@hotmail.com>
2014-10-11 21:04:08 +0100

    College projects

---
+++ Team C OOP Project Upload/Team C OOP Project/Images/ComputerSupportApp/SENDTODAVE/ComputerSupportApplication/src/computersupportapplication/ComputerSupport.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computersupportapplication;
+
+/**
+ *
+ * @author John Mc Dermott
+ */
+public class ComputerSupport {
+    
+}
---
+++ Team C OOP Project Upload/Team C OOP Project/StudentServicesApplication/src/studentservicesapplication/StudentServicesApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentservicesapplication;
+
+/**
+ *
+ * @author x12112267
+ */
+public class StudentServicesApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       MainPage myMainPage = new MainPage();
+       myMainPage.setVisible(true);
+       myMainPage.setTitle("Main Page");
+       
+       
+    }
+}
